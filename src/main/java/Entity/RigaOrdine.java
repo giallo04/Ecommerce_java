46cc33d9907d6705a4cee5f57c8eca4e999683b1
luckyG4 <gennarocelaia04@gmail.com>
2026-05-31 11:06:13 +0200

    commit riga ordine

--- src/main/java/Entity/RigaOrdine.java
+++ src/main/java/Entity/RigaOrdine.java
@@ -1,4 +1,37 @@
 package Entity;
 
+import java.util.Objects;
+
 public class RigaOrdine {
+    Prodotto prodotto;
+    int qtaProdotto;
+
+    public RigaOrdine(Prodotto prodotto, int qtaProdotto) {
+        this.prodotto = prodotto;
+        this.qtaProdotto = qtaProdotto;
+    }
+
+    public int getQtaProdotto() {
+        return qtaProdotto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof RigaOrdine that)) return false;
+        return qtaProdotto == that.qtaProdotto && Objects.equals(prodotto, that.prodotto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prodotto, qtaProdotto);
+    }
+
+    @Override
+    public String toString() {
+        return "RigaOrdine{" +
+                "prodotto=" + prodotto +
+                ", qtaProdotto=" + qtaProdotto +
+                '}';
+    }
+
 }
